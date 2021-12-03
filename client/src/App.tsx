57e92a899d1e31fa52f094dbe022f15c27c4224f
Adrian Romanko <romankoadrian@gmail.com>
2021-12-03 00:03:47 -0500

    apply geometry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
-import React from "react";
-import logo from "./logo.png";
-import "./App.css";
+// import logo from './logo.png';
+import './App.css';
+import { create } from './animation/kuji';
+import { useEffect, useRef } from 'react';
+// import logo from './logo.png'
+const animation = create();
 
 function App() {
+  const rendererRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    rendererRef.current?.appendChild(animation.renderer.domElement);
+    animation.start();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Kujibet</p>
-      </header>
+    <div
+      ref={rendererRef}
+      className="webgl-shell"
+      style={{ display: 'flex', width: '100%', height: '100%' }}
+    >
+ 
     </div>
   );
 }
+
 
 export default App;
